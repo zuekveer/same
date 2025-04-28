a0a9b2a34bd07df518502ce8314e6569f3de5406
zuekveer <simetramonk@gmail.com>
2025-04-28 19:21:53 +0300

    fxd cache and added set

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -12,14 +12,20 @@ import (
 type Decorator struct {
 	repo repository.UserProvider
 	mu   sync.RWMutex
-	data map[string]models.User
+	data map[string]*models.User
 }
 
 func NewDecorator(repo repository.UserProvider) *Decorator {
 	return &Decorator{
 		repo: repo,
-		data: make(map[string]models.User),
+		data: make(map[string]*models.User),
 	}
+}
+
+func (c *Decorator) Set(user *models.User) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.data[user.ID] = user
 }
 
 func (c *Decorator) Get(id string) (*models.User, error) {
@@ -28,52 +34,52 @@ func (c *Decorator) Get(id string) (*models.User, error) {
 	c.mu.RUnlock()
 	if ok {
 		logger.Logger.Info("Cache hit", "userID", id)
-		return &user, nil
+		return user, nil
 	}
 
-	user, err := c.repo.Get(id)
+	userFromRepo, err := c.repo.Get(id)
 	if err != nil {
 		return nil, err
 	}
 
-	c.mu.Lock()
-	c.data[id] = user
-	c.mu.Unlock()
+	c.Set(userFromRepo)
 	logger.Logger.Info("Cache miss - loaded from repo", "userID", id)
-	return &user, nil
+	return userFromRepo, nil
 }
 
 func (c *Decorator) GetAll(limit, offset int) ([]*models.User, error) {
 	return c.repo.GetAll(limit, offset)
 }
 
-func (c *Decorator) Create(user models.User) (string, error) {
-	id, err := c.repo.Create(&user)
-	if err == nil {
-		c.mu.Lock()
-		user.ID = id
-		c.data[id] = user
-		c.mu.Unlock()
+func (c *Decorator) Create(user *models.User) (string, error) {
+	id, err := c.repo.Create(user)
+	if err != nil {
+		return "", err
 	}
-	return id, err
+
+	user.ID = id
+	c.Set(user)
+	return id, nil
 }
 
 func (c *Decorator) Update(user *models.User) error {
 	err := c.repo.Update(user)
-	if err == nil {
-		c.mu.Lock()
-		c.data[user.ID] = *user
-		c.mu.Unlock()
+	if err != nil {
+		return err
 	}
-	return err
+
+	c.Set(user)
+	return nil
 }
 
 func (c *Decorator) Delete(ctx context.Context, id string) error {
 	err := c.repo.Delete(ctx, id)
-	if err == nil {
-		c.mu.Lock()
-		delete(c.data, id)
-		c.mu.Unlock()
+	if err != nil {
+		return err
 	}
-	return err
+
+	c.mu.Lock()
+	delete(c.data, id)
+	c.mu.Unlock()
+	return nil
 }
