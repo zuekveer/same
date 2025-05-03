827cf78325aa74e55956a47370ccb3fe356a336a
zuekveer <simetramonk@gmail.com>
2025-05-03 18:13:20 +0300

    upd

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -19,11 +19,11 @@ import (
 )
 
 type UserHandler interface {
-	CreateUser(c *fiber.Ctx) error
-	UpdateUser(c *fiber.Ctx) error
-	GetUser(c *fiber.Ctx) error
-	DeleteUser(c *fiber.Ctx) error
-	GetAllUsers(c *fiber.Ctx) error
+	CreateUser(ctx *fiber.Ctx) error
+	UpdateUser(ctx *fiber.Ctx) error
+	GetUser(ctx *fiber.Ctx) error
+	DeleteUser(ctx *fiber.Ctx) error
+	GetAllUsers(ctx *fiber.Ctx) error
 }
 
 type Handler struct {
@@ -75,104 +75,104 @@ func MetricsHandler() http.Handler {
 	return promhttp.Handler()
 }
 
-func (h *Handler) CreateUser(c *fiber.Ctx) error {
+func (h *Handler) CreateUser(ctx *fiber.Ctx) error {
 	var req models.CreateUserRequest
-	if err := c.BodyParser(&req); err != nil || req.Name == "" || req.Age <= 0 {
+	if err := ctx.BodyParser(&req); err != nil || req.Name == "" || req.Age <= 0 {
 		slog.Info("CreateUser: Invalid input", "error", err)
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid input"})
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid input"})
 	}
-
+	//TODO: should be used ctx.Context from context or fiber?
 	user := models.ToEntityFromCreate(req)
-	id, err := h.userUC.CreateUser(&user)
+	id, err := h.userUC.CreateUser(ctx.UserContext(), &user)
 	if err != nil {
 		slog.Info("CreateUser: Failed to create user", "user", user, "error", err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
 
 	slog.Info("CreateUser: User created", "id", id)
-	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"id": id})
+	return ctx.Status(fiber.StatusCreated).JSON(fiber.Map{"id": id})
 }
 
-func (h *Handler) UpdateUser(c *fiber.Ctx) error {
+func (h *Handler) UpdateUser(ctx *fiber.Ctx) error {
 	var req models.UpdateUserRequest
-	if err := c.BodyParser(&req); err != nil || req.ID == "" || req.Name == "" || req.Age <= 0 {
+	if err := ctx.BodyParser(&req); err != nil || req.ID == "" || req.Name == "" || req.Age <= 0 {
 		slog.Info("UpdateUser: Invalid input", "error", err)
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid input"})
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid input"})
 	}
 	if _, err := uuid.Parse(req.ID); err != nil {
 		slog.Info("UpdateUser: Invalid UUID", "id", req.ID, "error", err)
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid UUID"})
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid UUID"})
 	}
 
 	user := models.ToEntityFromUpdate(req)
-	if err := h.userUC.UpdateUser(&user); err != nil {
+	if err := h.userUC.UpdateUser(ctx.UserContext(), &user); err != nil {
 		if errors.Is(err, apperr.ErrNotFound) {
 			slog.Info("UpdateUser: User not found", "id", req.ID)
-			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "User not found"})
+			return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "User not found"})
 		}
 		slog.Info("UpdateUser: Failed to update user", "id", req.ID, "error", err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal server error"})
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal server error"})
 	}
 
 	slog.Info("UpdateUser: User updated", "id", req.ID)
-	return c.JSON(fiber.Map{"id": req.ID})
+	return ctx.JSON(fiber.Map{"id": req.ID})
 }
 
-func (h *Handler) GetUser(c *fiber.Ctx) error {
-	id := c.Params("id")
+func (h *Handler) GetUser(ctx *fiber.Ctx) error {
+	id := ctx.Params("id")
 	if _, err := uuid.Parse(id); err != nil {
 		slog.Info("GetUser: Invalid UUID", "id", id, "error", err)
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid UUID"})
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid UUID"})
 	}
 
 	user, err := h.userUC.GetUser(id)
 	if err != nil {
 		if errors.Is(err, apperr.ErrNotFound) {
 			slog.Info("GetUser: User not found", "id", id)
-			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "User not found"})
+			return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "User not found"})
 		}
 		slog.Info("GetUser: Failed to get user", "id", id, "error", err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal server error"})
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal server error"})
 	}
 
 	slog.Info("GetUser: User found", "id", id)
-	return c.JSON(user.ToResponse())
+	return ctx.JSON(user.ToResponse())
 }
 
-func (h *Handler) DeleteUser(c *fiber.Ctx) error {
-	id := c.Params("id")
+func (h *Handler) DeleteUser(ctx *fiber.Ctx) error {
+	id := ctx.Params("id")
 	if _, err := uuid.Parse(id); err != nil {
 		slog.Info("DeleteUser: Invalid UUID", "id", id, "error", err)
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid UUID"})
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid UUID"})
 	}
 
 	if err := h.userUC.DeleteUser(context.Background(), id); err != nil {
 		if errors.Is(err, apperr.ErrNotFound) {
 			slog.Info("DeleteUser: User not found", "id", id)
-			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "User not found"})
+			return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "User not found"})
 		}
 		slog.Info("DeleteUser: Failed to delete user", "id", id, "error", err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal server error"})
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal server error"})
 	}
 
 	slog.Info("DeleteUser: User deleted", "id", id)
-	return c.SendStatus(fiber.StatusNoContent)
+	return ctx.SendStatus(fiber.StatusNoContent)
 }
 
-func (h *Handler) GetAllUsers(c *fiber.Ctx) error {
-	limit, err1 := strconv.Atoi(c.Query("limit", "10"))
-	offset, err2 := strconv.Atoi(c.Query("offset", "0"))
+func (h *Handler) GetAllUsers(ctx *fiber.Ctx) error {
+	limit, err1 := strconv.Atoi(ctx.Query("limit", "10"))
+	offset, err2 := strconv.Atoi(ctx.Query("offset", "0"))
 	if err1 != nil || err2 != nil || limit <= 0 || offset < 0 {
 		slog.Info("GetAllUsers: Invalid pagination parameters", "limit", limit, "offset", offset)
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid pagination params"})
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid pagination params"})
 	}
 
-	users, err := h.userUC.GetAllUsers(limit, offset)
+	users, err := h.userUC.GetAllUsers(ctx.UserContext(), limit, offset)
 	if err != nil {
 		slog.Info("GetAllUsers: Failed to retrieve users", "limit", limit, "offset", offset, "error", err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
 
 	slog.Info("GetAllUsers: Users retrieved", "count", len(users))
-	return c.JSON(users)
+	return ctx.JSON(users)
 }
